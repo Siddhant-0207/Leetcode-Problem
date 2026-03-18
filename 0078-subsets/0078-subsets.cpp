d605ce8f2d40e74e421d2b1b0263326d43478d46
Siddhant Mishra <123siddhantm@gmail.com>
2026-03-18 12:03:07 +0530

    Time: 0 ms (100%), Space: 10 MB (60.53%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        int subset = 1<<n;
-        vector<vector<int>> ans;
-        for (int num=0;num<=subset-1;num++)
+    void helper (int ind, vector<int> &ds, vector<int> &nums , int n,vector<vector<int>>& ans)
+    {
+        if(ind==n)
         {
-            vector<int> ls;
-            for(int i =0 ; i<n;i++)
-            {
-                if((num&(1<<i))!=0) ls.push_back(nums[i]);
-
-            }
-            ans.push_back(ls);
+            ans.push_back(ds);
+            
+            return ;
         }
-        return ans;
+
+
+        //pick
+        ds.push_back(nums[ind]);
+        helper(ind+1,ds,nums,n,ans);
+        ds.pop_back();
+
+        //not pick
+        helper(ind+1,ds , nums,n,ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<vector<int>> ans;
+       vector<int> ds;
+       int n = nums.size();
+       helper(0,ds,nums,n,ans);
+       return ans;
     }
 };
+    
