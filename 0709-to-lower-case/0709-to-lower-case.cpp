9b1c9394dd943b709cf134a7712914ef2f2a5658
Siddhant Mishra <123siddhantm@gmail.com>
2026-09-11 00:01:58 +0530

    Time: 0 ms (100%), Space: 8.1 MB (98.78%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        transform(s.begin(), s.end(), s.begin(), [](unsigned char c){
-            return tolower(c);
-        });
-    return s;
+          for (int i = 0; i < s.size(); i++) {
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] = s[i] + 32;   
+            }
+        }
+        return s;
     }
    
 };
